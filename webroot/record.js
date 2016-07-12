049a9112b89d91082ce5c187ec6e85a13dc7edd7
Chris van Marle <qistoph@gmail.com>
2016-07-12 14:15:51 +0200

    Integrate recording

--- webroot/record.js
+++ webroot/record.js
@@ -1,5 +1,5 @@
-function __log(e, data) {
-  console.log(e + " " + (data || ''));
+function recStatus(msg) {
+  $('#recStatus').text(msg);
 }
 
 var audio_context;
@@ -7,31 +7,34 @@ var recorder;
 
 function startUserMedia(stream) {
   var input = audio_context.createMediaStreamSource(stream);
-  __log('Media stream created.' );
-  __log("input sample rate " +input.context.sampleRate);
+  console.debug('Media stream created.' );
+  console.debug("input sample rate " +input.context.sampleRate);
 
   // Feedback!
   //input.connect(audio_context.destination);
-  __log('Input connected to audio context destination.');
+  console.debug('Input connected to audio context destination.');
 
   recorder = new Recorder(input, {
     numChannels: 1
   });
-  __log('Recorder initialised.');
+  console.debug('Recorder initialised.');
+
+  startRecording();
 }
 
-function startRecording(button) {
+function startRecording() {
   recorder && recorder.record();
-  //button.disabled = false;
-  //button.nextElementSibling.disabled = false;
-  __log('Recording...');
+  recStatus('Recording...');
+}
+
+function abortRecording(button) {
+  recorder && recorder.stop();
+  recStatus('Recording aborted.');
 }
 
 function stopRecording(button) {
   recorder && recorder.stop();
-  //button.disabled = true;
-  //button.previousElementSibling.disabled = false;
-  __log('Stopped recording.');
+  recStatus('Stopped recording.');
 
   // create WAV download link using audio data blob
   createDownloadLink();
@@ -40,27 +43,15 @@ function stopRecording(button) {
 }
 
 function createDownloadLink() {
-  __log(recorder.exportWAV);
-  recorder && recorder.exportWAV(function(blob) {
-    __log(blob);
-    /*var url = URL.createObjectURL(blob);
-    var li = document.createElement('li');
-    var au = document.createElement('audio');
-    var hf = document.createElement('a');
-
-    au.controls = true;
-    au.src = url;
-    hf.href = url;
-    hf.download = new Date().toISOString() + '.wav';
-    hf.innerHTML = hf.download;
-    li.appendChild(au);
-    li.appendChild(hf);
-    document.body.appendChild(li);
-	*/
+  recorder && recorder.exportWAV(function(b64data) {
+    recStatus('Sending...');
+    console.log("< playrec: [data]");
+    socket.emit('playrec', b64data);
+    recStatus('Done');
   });
 }
 
-$(document).ready(function() {
+function initRecorder(startRecording = false) {
   try {
     // webkit shim
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -71,14 +62,42 @@ $(document).ready(function() {
     window.URL = window.URL || window.webkitURL;
 
     audio_context = new AudioContext;
-    __log('Audio context set up.');
-    __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+    console.debug('Audio context set up.');
+    console.debug('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
   } catch (e) {
     alert('No web audio support in this browser!');
   }
 
   navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
-    __log('No live audio input: ' + e);
+    alert('No live audio input: ' + e);
+    console.debug(e)
+  });
+}
+
+$(document).ready(function() {
+  socket.on('playrec', function(data) {
+    console.log("> playrec: [data] (" + data.user + ")");
+    var url = 'data:audio/mp3;base64,'+data.audio;
+    var snd = new Audio(url);
+    snd.play();
+  });
+
+  $('#btnRecord').mousedown(function() {
+    $('#btnRecord').mousedown(function() {
+      startRecording();
+    });
+    initRecorder(true);
+  });
+
+  $('#btnRecord').mouseup(function() {
+    stopRecording();
+  });
+
+  $(document).mouseup(function(e) {
+    var buttons = $('#btnRecord');
+    if(!buttons.is(e.target)) {
+      abortRecording();
+    }
   });
 });
 
